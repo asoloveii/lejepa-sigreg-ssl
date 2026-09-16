c6edc1b72e7fa7ac02a7f7af2c9bc95e1712352c
Arseni Salavei <arsenisoloveii@gmail.com>
2026-09-16 12:44:57 +0200

    fix: moved Derf into vit implementation

--- src/models/vit.py
+++ src/models/vit.py
@@ -1,7 +1,5 @@
 import torch, torch.nn as nn, torch.nn.functional as F
 
-from .module import Derf
-
 
 def create_rope_embeddings(max_seq_len, head_dim, theta):
     inv_freqs = 1 / (theta ** torch.arange(0, head_dim, 2).float() / head_dim)
@@ -25,6 +23,22 @@ def apply_rope(x, rope_embs):
     x_partner = x_partner.flatten(-2)
 
     return (x * cos + x_partner * sin).type_as(x)
+
+
+class Derf(nn.Module):
+    """Dynamic erf layer with learnable shift and scale operations"""
+    
+    def __init__(self, d_model, init_shift, init_scale):
+        super().__init__()
+
+        self.shift = nn.Parameter(torch.ones(1)*init_shift, requires_grad=True)
+        self.scale = nn.Parameter(torch.ones(1)*init_scale, requires_grad=True)
+
+        self.gamma = nn.Parameter(torch.ones(d_model), requires_grad=True)
+        self.beta = nn.Parameter(torch.zeros(d_model), requires_grad=True)
+
+    def forward(self, x: torch.Tensor):
+        return self.gamma * torch.erf(self.shift * x + self.scale) + self.beta
 
 
 class PatchEmbedding(nn.Module):
