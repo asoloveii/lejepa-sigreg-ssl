31f43123ff659f5b7ff5410b24e54a417a918b75
Arseni Salavei <arsenisoloveii@gmail.com>
2026-09-17 13:19:51 +0200

    feat: add custom hf dataset

---
+++ src/data.py
@@ -0,0 +1,41 @@
+import torch 
+from torchvision.transforms import v2
+
+from datasets import load_dataset
+
+
+class HFDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_name="frgfm/imagenette", split="train", V=4):
+        self.V = V
+        self.ds = load_dataset(dataset_name, "160px", split=split)
+        self.aug = v2.Compose(
+            [
+                v2.RandomResizedCrop(128, scale=(0.08, 1.0)),
+                v2.RandomApply([v2.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
+                v2.RandomGrayscale(p=0.2),
+                v2.RandomApply([v2.GaussianBlur(kernel_size=7, sigma=(0.1, 2.0))]),
+                v2.RandomApply([v2.RandomSolarize(threshold=128)], p=0.2),
+                v2.RandomHorizontalFlip(),
+                v2.ToImage(),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        self.test = v2.Compose(
+            [
+                v2.Resize(128),
+                v2.CenterCrop(128),
+                v2.ToImage(),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+    def __getitem__(self, i):
+        item = self.ds[i]
+        img = item["image"].convert("RGB")
+        transform = self.aug if self.V > 1 else self.test
+        return torch.stack([transform(img) for _ in range(self.V)]), item["label"]
+
+    def __len__(self):
+        return len(self.ds)
